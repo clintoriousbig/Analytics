76eb36f4016a7b0ed9885dc657a1f52b9d4e6955
Clint Gilmore <cjgilmore1@hotmail.com>
2025-07-03 22:43:43 +1000

    wip - new models

---
+++ analytics_project/models/xau_1d_clean.sql
@@ -0,0 +1,8 @@
+SELECT
+  CAST(time AS TIMESTAMP) AS time,
+  CAST(open AS DOUBLE) AS open,
+  CAST(close AS DOUBLE) AS close,
+  CAST(high AS DOUBLE) AS high,
+  CAST(low AS DOUBLE) AS low,
+  CAST(volume AS DOUBLE) AS volume
+FROM {{ source('market_data', 'XAU_1d') }}
